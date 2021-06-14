95363bffec6fe4c8c5fa222e62ba8a177cf1778c
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-06-14 21:23:21 +0200

    added buttons

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'what\'s your favourite color?',
+      'what\'s your favourite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(children: [
+          Text('The question!'),
+          RaisedButton(child: Text('Answer 1'), onPressed: null),
+          RaisedButton(child: Text('Answer 2'), onPressed: null),
+          RaisedButton(child: Text('Answer 3'), onPressed: null),
+        ]),
       ),
     );
   }
