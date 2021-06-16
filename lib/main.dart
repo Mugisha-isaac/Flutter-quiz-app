903da2c174e515b93e4d10223125d5634879e34a
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-06-16 14:23:22 +0200

    lifting up the state

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -37,18 +38,9 @@ class _MyAppState extends State<MyApp> {
           Question(
             questions[_questionIndex],
           ),
-          RaisedButton(
-            child: Text('Answer 1'),
-            onPressed: _answerQuestion,
-          ),
-          RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print('answer 2 choosen')),
-          RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () {
-                print('answer 3 choosen');
-              }),
+          Answer(_answerQuestion),
+          Answer(_answerQuestion),
+          Answer(_answerQuestion),
         ]),
       ),
     );
